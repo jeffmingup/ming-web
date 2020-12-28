a953bd0546d24e04b9fd8bf71ad8aa884669f5f6
ming <1960588251@qq.com>
2020-12-28 22:05:24 +0800

    路由分组

--- main.go
+++ main.go
@@ -7,22 +7,34 @@ import (
 
 func main() {
 	r := ming.New()
-	r.GET("/", func(c *ming.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(c *ming.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *ming.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
 
-	r.GET("/hello", func(c *ming.Context) {
-		// expect /hello?name=mingktutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *ming.Context) {
+			// expect /hello?name=mingktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *ming.Context) {
+			// expect /hello/mingktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *ming.Context) {
+			c.JSON(http.StatusOK, ming.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *ming.Context) {
-		// expect /hello/mingktutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+	}
 
-	r.GET("/assets/*filepath", func(c *ming.Context) {
-		c.JSON(http.StatusOK, ming.H{"filepath": c.Param("filepath")})
-	})
 	r.Run(":9999")
 }
--- ming/ming.go
+++ ming/ming.go
@@ -1,32 +1,56 @@
 package ming
 
 import (
+	"log"
 	"net/http"
 )
 
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
+}
+
 type HandlerFunc func(ctx *Context)
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
-
-func (engine *Engine) addRouter(method string, pattern string, handlerFunc HandlerFunc) {
-	engine.router.addRoute(method, pattern, handlerFunc)
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
-func (engine *Engine) GET(pattern string, handlerFunc HandlerFunc) {
-	engine.addRouter("GET", pattern, handlerFunc)
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
-func (engine *Engine) POST(pattern string, handlerFunc HandlerFunc) {
-	engine.addRouter("POST", pattern, handlerFunc)
+func (group *RouterGroup) GET(pattern string, handlerFunc HandlerFunc) {
+	group.addRoute("GET", pattern, handlerFunc)
 }
-func (engine *Engine) PUT(pattern string, handlerFunc HandlerFunc) {
-	engine.addRouter("PUT", pattern, handlerFunc)
+func (group *RouterGroup) POST(pattern string, handlerFunc HandlerFunc) {
+	group.addRoute("POST", pattern, handlerFunc)
 }
-func (engine *Engine) DELETE(pattern string, handlerFunc HandlerFunc) {
-	engine.addRouter("DELETE", pattern, handlerFunc)
+func (group *RouterGroup) PUT(pattern string, handlerFunc HandlerFunc) {
+	group.addRoute("PUT", pattern, handlerFunc)
+}
+func (group *RouterGroup) DELETE(pattern string, handlerFunc HandlerFunc) {
+	group.addRoute("DELETE", pattern, handlerFunc)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
